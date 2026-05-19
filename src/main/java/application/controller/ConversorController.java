e437418a35cd4c8a0346e64bfc63224c05cebef2
miguel-lima-tech <miguelvaqueirogamet@gmail.com>
2026-05-19 15:15:56 -0300

    Implementa binding no controller

--- src/main/java/application/controller/ConversorController.java
+++ src/main/java/application/controller/ConversorController.java
@@ -1,5 +1,15 @@
 package application.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class ConversorController {
+
+    @FXML
+    private TextField txtCelsius;
+
+    @FXML
+    private Label lblResultado;
 
 }
